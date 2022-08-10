d4e8d22503642786a5ffd5b60ce7bd3d21f68714
SS Insane <ssinsaneyt@gmail.com>
2022-08-11 00:08:31 +0530

    made game names more understandable

--- hive.py
+++ hive.py
@@ -1,16 +1,31 @@
-import requests
 import json
+import requests
 from hiveMCBE.errors import *
+
+base_url = "https://api.playhive.com/v0/game"
+games = {
+					"TREASURE_WARS": "wars", 
+					"DEATH_RUN": "dr", 
+					"HIDE_AND_SEEK": "hide", 
+					"SURVIVAL_GAMES": "sg", 
+					"MURDER_MYSTERY": "murder", 
+					"SKY_WARS": "sky", 
+					"CAPTURE_THE_FLAG": "ctf", 
+					"BLOCK_DROP": "drop", 
+					"GROUND_WARS": "ground", 
+					"JUST_BUILD": "build"
+				}
+
 
 class HivePlayer:
 	def __init__(self, username):
 		self.username = username
-		self.games = ["wars", "dr", "hide", "sg", "murder", "sky", "ctf", "drop", "ground", "build"]
+		self.games = games
 	
 	#for getting player statistics for a minigame
 	def stats(self, game):
-		if game in self.games:
-			r = requests.get(f"https://api.playhive.com/v0/game/all/{game}/{self.username}")
+		if game in self.games.keys():
+			r = requests.get(f"{base_url}/all/{self.games[game]}/{self.username}")
 			if r.ok == True:
 				return(json.loads(r.text)) #converting json text to dict
 			elif r.status_code == 500:
@@ -22,8 +37,8 @@ class HivePlayer:
 	
 	#for getting player entry in current monthly leaderboard
 	def monthly_leader(self,game):
-		if game in self.games:
-			r = requests.get(f"https://api.playhive.com/v0/game/monthly/player/{game}/{self.username}")
+		if game in self.games.keys():
+			r = requests.get(f"{base_url}/monthly/player/{self.games[game]}/{self.username}")
 			if r.ok == True:
 				return(json.loads(r.text))
 			elif r.status_code == 500:
@@ -35,8 +50,8 @@ class HivePlayer:
 	
 	#for getting player entry in leaderboard of a specific month
 	def specific_monthly_leader(self,game, year, month):
-		if game in self.games:
-			r = requests.get(f"https://api.playhive.com/v0/game/monthly/player/{game}/{self.username}/{year}/{month}")
+		if game in self.games.keys():
+			r = requests.get(f"{base_url}/monthly/player/{self.games[game]}/{self.username}/{year}/{month}")
 			if r.ok == True:
 				return(json.loads(r.text))
 			elif r.status_code == 500:
@@ -46,17 +61,17 @@ class HivePlayer:
 		else:
 			raise MinigameNotFound(f'Minigame "{game}" does not exist')
 
+
 #for getting monthly leaderboard or all time leaderboard if all_time == True
 def leaderboard(game, count = 50, all_time = False):
-	games = ["wars", "dr", "hide", "sg", "murder", "sky", "ctf", "drop", "ground", "build"]
 	if all_time == True:
 		type = "all"
 	else:
 		type = "monthly"
 	if count > 50:
 		raise LimitExceeded("cannot fetch more than 50 entries in leaderboard")
-	if game in games:
-		r = requests.get(f"https://api.playhive.com/v0/game/{type}/{game}")
+	if game in games.keys():
+		r = requests.get(f"{base_url}/{type}/{games[game]}")
 		if r.ok == True:
 			lb = json.loads(r.text)
 			return(lb[0:count]) #slicing the dict acc. to user specified length
@@ -67,11 +82,10 @@ def leaderboard(game, count = 50, all_time = False):
 
 #for getting leaderboard for a specific month
 def specific_leaderboard(game, year, month, count = 50):
-	games = ["wars", "dr", "hide", "sg", "murder", "sky", "ctf", "drop", "ground", "build"]
 	if count > 50:
 		raise LimitExceeded("cannot fetch more than 50 entries in leaderboard")
-	if game in games:
-		r = requests.get(f"https://api.playhive.com/v0/game/monthly/{game}/{year}/{month}/{count}/0")
+	if game in games.keys():
+		r = requests.get(f"{base_url}/monthly/{games[game]}/{year}/{month}/{count}/0")
 		if r.ok == True:
 			return(json.loads(r.text))
 		elif r.status_code == 500:
