2bc8a88d3940df49db7d31a97012f00d4cd8a8e7
SS Insane <ssinsaneyt@gmail.com>
2022-08-11 07:43:14 +0530

    better structure

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from hiveMCBE.player import HivePlayer
+from hiveMCBE.leaderboard import leaderboard, specific_leaderboard
+from hiveMCBE.var import base_url, games
+from hiveMCBE.errors import HiveError, PlayerNotFound, MinigameNotFound, InternalServerError, LimitExceeded
---
+++ var.py
@@ -0,0 +1,13 @@
+base_url = "https://api.playhive.com/v0/game"
+games = {
+					"TREASURE_WARS": "wars", 
+					"DEATH_RUN": "dr", 
+					"HIDE_AND_SEEK": "hide", 
+					"SURVIVAL_GAMES": "sg", 
+					"MURDER_MYSTERY": "murder", 
+					"SKY_WARS": "sky", 
+					"CAPTURE_THE_FLAG": "ctf", 
+					"BLOCK_DROP": "drop", 
+					"GROUND_WARS": "ground", 
+					"JUST_BUILD": "build"
+				}
