36c4fabcc34641f767330e31b34472ed73b872e3
SS Insane <ssinsaneyt@gmail.com>
2022-08-12 21:48:40 +0530

    made api request functions asyncronous

--- leaderboard.py
+++ leaderboard.py
@@ -3,32 +3,32 @@ import requests
 import HiveMCBE as hive
 
 #for getting monthly leaderboard or all time leaderboard if all_time == True
-def leaderboard(game, count = 50, all_time = False):
+async def leaderboard(game, count = 50, all_time = False):
 	if all_time == True:
 		type = "all"
 	else:
 		type = "monthly"
 	if count > 50:
-		raise LimitExceeded("cannot fetch more than 50 entries in leaderboard")
+		raise hive.LimitExceeded("cannot fetch more than 50 entries in leaderboard")
 	if game in hive.games.keys():
 		r = requests.get(f"{hive.base_url}/{type}/{hive.games[game]}")
 		if r.ok == True:
 			lb = json.loads(r.text)
 			return(lb[0:count]) #slicing the dict acc. to user specified length
 		elif r.status_code == 500:
-			raise InternalServerError("Hive API returned status code 500")
+			raise hive.InternalServerError("Hive API returned status code 500")
 	else:
-		raise MinigameNotFound(f'Minigame "{game}" does not exist')
+		raise hive.MinigameNotFound(f'Minigame "{game}" does not exist')
 
 #for getting leaderboard for a specific month
-def specific_leaderboard(game, year, month, count = 50):
+async def specific_leaderboard(game, year, month, count = 50):
 	if count > 50:
-		raise LimitExceeded("cannot fetch more than 50 entries in leaderboard")
+		raise hive.LimitExceeded("cannot fetch more than 50 entries in leaderboard")
 	if game in hive.games.keys():
 		r = requests.get(f"{hive.base_url}/monthly/{hive.games[game]}/{year}/{month}/{count}/0")
 		if r.ok == True:
 			return(json.loads(r.text))
 		elif r.status_code == 500:
-			raise InternalServerError("Hive API returned status code 500")
+			raise hive.InternalServerError("Hive API returned status code 500")
 	else:
-		raise MinigameNotFound(f'Minigame "{game}" does not exist')
+		raise hive.MinigameNotFound(f'Minigame "{game}" does not exist')
--- player.py
+++ player.py
@@ -7,43 +7,43 @@ class HivePlayer:
 	def __init__(self, username):
 		self.username = username
 		self.games = hive.games
-		self.self.base_url = hive.self.base_url
+		self.base_url = hive.base_url
 	
 	#for getting player statistics for a minigame
-	def stats(self, game):
+	async def stats(self, game):
 		if game in self.games.keys():
 			r = requests.get(f"{self.base_url}/all/{self.games[game]}/{self.username}")
 			if r.ok == True:
 				return(json.loads(r.text)) #converting json text to dict
 			elif r.status_code == 500:
-				raise InternalServerError("Hive API returned status code 500")
+				raise hive.InternalServerError("Hive API returned status code 500")
 			else:
-				raise PlayerNotFound(f'Player "{self.username}" was found in Hive database')
+				raise hive.PlayerNotFound(f'Player "{self.username}" was found in Hive database')
 		else:
-			raise MinigameNotFound(f'Minigame "{game}" does not exist')
+			raise hive.MinigameNotFound(f'Minigame "{game}" does not exist')
 	
 	#for getting player entry in current monthly leaderboard
-	def monthly_leader(self,game):
+	async def monthly_leader(self,game):
 		if game in self.games.keys():
 			r = requests.get(f"{self.base_url}/monthly/player/{self.games[game]}/{self.username}")
 			if r.ok == True:
 				return(json.loads(r.text))
 			elif r.status_code == 500:
-				raise InternalServerError("Hive API returned status code 500")
+				raise hive.InternalServerError("Hive API returned status code 500")
 			else:
-				raise PlayerNotFound(f'Player "{self.username}" was not found in monthly leaderboard for game "{game}"')
+				raise hive.PlayerNotFound(f'Player "{self.username}" was not found in monthly leaderboard for game "{game}"')
 		else:
-			raise MinigameNotFound(f'Minigame "{game}" does not exist')
+			raise hive.MinigameNotFound(f'Minigame "{game}" does not exist')
 	
 	#for getting player entry in leaderboard of a specific month
-	def specific_monthly_leader(self,game, year, month):
+	async def specific_monthly_leader(self,game, year, month):
 		if game in self.games.keys():
 			r = requests.get(f"{self.base_url}/monthly/player/{self.games[game]}/{self.username}/{year}/{month}")
 			if r.ok == True:
 				return(json.loads(r.text))
 			elif r.status_code == 500:
-				raise InternalServerError("Hive API returned status code 500")
+				raise hive.InternalServerError("Hive API returned status code 500")
 			else:
-				raise PlayerNotFound(f'Player "{self.username}" was not found in {month}/{year} monthly leaderboard for game "{game}"')
+				raise hive.PlayerNotFound(f'Player "{self.username}" was not found in {month}/{year} monthly leaderboard for game "{game}"')
 		else:
-			raise MinigameNotFound(f'Minigame "{game}" does not exist')
+			raise hive.MinigameNotFound(f'Minigame "{game}" does not exist')
